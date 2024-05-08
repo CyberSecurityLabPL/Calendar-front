fe19159a94eed8352647353e53d2d29790960669
Tomasz Biel <biel535@gmail.com>
2024-05-08 12:46:49 +0200

    options added

---
+++ src/app/components/inputs/SelectInput.tsx
@@ -0,0 +1,22 @@
+
+
+const ROLES = [
+    { value: 'ROLE_USER', label: 'Użytkownik' },
+    { value: 'ROLE_MANAGER', label: 'Manager' },
+    { value: 'ROLE_ADMIN', label: 'Administrator' }
+];
+
+const CONTRACTS = [
+    { value: 'UMOWA_ZLECENIE', label: 'Umowa zlecenie' },
+    { value: 'UMOWA_O_PRACE', label: 'Umowa o pracę' }
+];
+
+const COMPANIES = [
+    { value: 'e946d05a-fd51-421b-add2-db6eb7be4ba6', label: 'CSL' }
+];
+
+
+
+export {  ROLES, CONTRACTS, COMPANIES };
+
+
